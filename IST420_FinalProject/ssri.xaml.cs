36dd7fb24667850d1f34a15ca8fe9436bc0bf1e8
rpm5735 <62289979+rpm5735@users.noreply.github.com>
2021-04-03 19:50:38 -0400

    Add project files.

---
+++ IST420_FinalProject/ssri.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IST420_FinalProject
+{
+    /// <summary>
+    /// Interaction logic for ssri.xaml
+    /// </summary>
+    public partial class ssri : Window
+    {
+        public ssri()
+        {
+            InitializeComponent();
+        }
+    }
+}
